0551bf7600108b2c43f11a69cfe5b986c223e5aa
Tella Adesina <telladesina100@gmail.com>
2021-07-08 09:24:24 -0500

    Commit08072021-SDA

--- DIGINET.CORE/Interface/API/ITransactionDetail.cs
+++ DIGINET.CORE/Interface/API/ITransactionDetail.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DIGINET.CORE.Entity;
+using DIGINET.CORE.Model.DTO.Request;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,8 @@ namespace DIGINET.CORE.Interface.API
 {
     public interface ITransactionDetail
     {
+        Task<TransactionDetail> GetTransactionDetails();
+        Task<TransactionDetail> CreateTransactionDetails(CreateTransactionRequest param);
+        Task<bool, string> SaveTransactionDetails(CreateTransactionRequest param);
     }
 }
---
+++ DIGINET.CORE/Model/DTO/Response/CreateTransactionResponse.cs
@@ -0,0 +1,27 @@
+﻿using DIGINET.CORE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIGINET.CORE.Model.DTO.Response
+{
+    public class CreateTransactionResponse
+    {
+        public Customer[] CustomerDetails { get; set; }
+        public int Customer_ID { get; set; }
+        public int AssetType_ID { get; set; }
+        public int TransferFromLedger_ID { get; set; }
+        public int TransferToLedger_ID { get; set; }
+        public int TransferCustomer_ID { get; set; }
+        public decimal TransferFrom_Quantity { get; set; }
+        public decimal TranferTo_Quantity { get; set; }
+        public decimal Conversion_Value { get; set; }
+        public string Transfer_Reason { get; set; }
+        public int Is_Active { get; set; }
+        public DateTime Creation_Date { get; set; }
+        public int Updated_By { get; set; }
+        public DateTime Updated_Date { get; set; }
+    }
+}
