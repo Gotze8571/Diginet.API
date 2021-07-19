11d878294588526f27d26f927396168b9e28ef3e
Tella Adesina <telladesina100@gmail.com>
2021-07-19 09:56:54 -0500

    Commit19072021-AB

---
+++ Diginet.Domain/Models/DTOs/CreateOrderMaster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diginet.Domain.Models.DTOs
+{
+    public class CreateOrderMaster
+    {
+        public int Customer_ID { get; set; }
+        public string ExternalSystemOrder_ID { get; set; }
+        public decimal Order_Amount { get; set; }
+        public DateTime Order_Date { get; set; }
+        public int Is_Active { get; set; }
+        public DateTime Creation_Date { get; set; }
+        public int Updated_By { get; set; }
+        public DateTime Updated_Date { get; set; }
+    }
+}
---
+++ Diginet.Web.API/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diginet.Web.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetOrder()
+        {
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+            return null;
+        }
+    }
+}
