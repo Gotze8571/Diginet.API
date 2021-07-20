6b1d7199f57ae716efe0fae1efa867758c4f4f86
Tella Adesina <telladesina100@gmail.com>
2021-07-20 18:41:34 -0500

    Commit20072021-Fg

--- Diginet.Web.API/Controllers/OrderController.cs
+++ Diginet.Web.API/Controllers/OrderController.cs
@@ -16,15 +16,11 @@ namespace Diginet.Web.API.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> GetOrder()
         {
-            //var rng = new Random();
-            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = rng.Next(-20, 55),
-            //    Summary = Summaries[rng.Next(Summaries.Length)]
-            //})
-            //.ToArray();
+            
             return null;
         }
+
+        
+
     }
 }
